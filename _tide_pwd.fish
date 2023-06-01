348310f20d454efc055714dc65ac60a5c024611c
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-06-01 18:27:24 +0800

    pwd icon is now included in the prompt again

--- _tide_pwd.fish
+++ _tide_pwd.fish
@@ -3,10 +3,10 @@ set_color $tide_pwd_color_truncated_dirs | read -l color_truncated
 
 set -l unwritable_icon $tide_pwd_icon_unwritable' '
 set -l home_icon $tide_pwd_icon_home' '
-set -l pwd_icon $tide_pwd_icon' '
+set -l pwd_icon $tide_pwd_icon
 
 eval "function _tide_pwd
 	set -l current_dir (string replace -r '^$HOME' '~' -- \$PWD | string split /)[-1]
 	set -g _tide_pwd_len (string length -V \$current_dir)
-	printf (set_color normal -b $tide_pwd_bg_color; set_color $tide_pwd_color_dirs)\$current_dir
+	printf (set_color normal -b $tide_pwd_bg_color; set_color $tide_pwd_color_dirs)\$tide_pwd_icon\$current_dir
 end"
